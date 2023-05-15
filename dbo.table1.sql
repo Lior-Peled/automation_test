b05104012a5aa578d9c8caf2c918dc3305277a59
Lior-Peled <83288600+Lior-Peled@users.noreply.github.com>
2023-05-15 12:43:28 +0300

    Add files via upload

---
+++ dbo.table1.sql
@@ -0,0 +1,6 @@
+
+if object_id('dbo.table1', 'U') IS NULL
+
+create table dbo.table1 (col1 int,  col2 varchar(100))
+
+
---
+++ dbo.table2.sql
@@ -0,0 +1,6 @@
+
+if object_id('dbo.table2', 'U') IS NULL
+
+create table dbo.table2 (column333 int,  column666 varchar(100), column888 varchar(100))
+
+
